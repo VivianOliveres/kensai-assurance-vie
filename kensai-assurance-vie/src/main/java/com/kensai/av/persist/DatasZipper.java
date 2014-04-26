b46df68ccd7bedd59bed08c1733f93370ff29f7d
VivianOliveres <v.oliveres@gmail.com>
2014-04-26 15:23:16 +0200

    Clean code

--- kensai-assurance-vie/src/main/java/com/kensai/av/persist/DatasZipper.java
+++ kensai-assurance-vie/src/main/java/com/kensai/av/persist/DatasZipper.java
@@ -1,19 +1,10 @@
 package com.kensai.av.persist;
 
 import java.io.IOException;
-import java.net.URI;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.StandardCopyOption;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -57,53 +48,4 @@ public class DatasZipper {
 		return !Files.exists(destZip);
 	}
 
-	private FileSystem createZipFileSystem(Path zipFolder, boolean create) throws IOException {
-		// convert the filename to a URI
-		URI uri = URI.create("jar:file:" + zipFolder.toUri().getPath());
-		Map<String, String> env = new HashMap<>();
-		if (create) {
-			env.put("create", "true");
-		}
-		return FileSystems.newFileSystem(uri, env);
-	}
-
-	private void zip() throws IOException {
-		try (FileSystem zipFileSystem = createZipFileSystem(destZip, true)) {
-			final Path root = zipFileSystem.getPath("/");
-
-			// iterate over the files we need to add
-			for (Path src : fromFolder) {
-				// add a file to the zip file system
-				if (!Files.isDirectory(src)) {
-					final Path dest = zipFileSystem.getPath(root.toString(), src.toString());
-					final Path parent = dest.getParent();
-					if (Files.notExists(parent)) {
-						System.out.printf("Creating directory %s\n", parent);
-						Files.createDirectories(parent);
-					}
-					Files.copy(src, dest, StandardCopyOption.REPLACE_EXISTING);
-				} else {
-					// for directories, walk the file tree
-					Files.walkFileTree(src, new SimpleFileVisitor<Path>() {
-						@Override
-						public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-							final Path dest = zipFileSystem.getPath(root.toString(), file.toString());
-							Files.copy(file, dest, StandardCopyOption.REPLACE_EXISTING);
-							return FileVisitResult.CONTINUE;
-						}
-
-						@Override
-						public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-							final Path dirToCreate = zipFileSystem.getPath(root.toString(), dir.toString());
-							if (Files.notExists(dirToCreate)) {
-								System.out.printf("Creating directory %s\n", dirToCreate);
-								Files.createDirectories(dirToCreate);
-							}
-							return FileVisitResult.CONTINUE;
-						}
-					});
-				}
-			}
-		}
-	}
 }
